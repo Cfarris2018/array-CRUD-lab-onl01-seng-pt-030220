a3be06eebe9afbf0558663b1c0859a0aacb80cc6
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-14 20:54:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,22 @@
 def create_an_empty_array
-  
+ []
 end
 
 def create_an_array
-  
+ ["chickens", "goats", "ducks", "lions"] 
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
+def add_element_to_end_of_array 
+  ["wow", "I", "am", "really", "learning"]
+end 
+add_element_to_end_of_array << "arrays!"
 
 def add_element_to_start_of_array(array, element)
-  
+ 
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
